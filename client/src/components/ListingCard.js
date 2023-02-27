87d38251cdb0ca6909cf20c1b9a543989050cb10
chystarr <chynnastarr2@gmail.com>
2023-02-27 15:00:40 -0500

    Set up file for a card component for the listings page

---
+++ client/src/components/ListingCard.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+function ListingCard() {
+  return (
+    <div>
+      <p>This is a listing card component</p>
+    </div>
+  );
+}
+
+export default ListingCard;
--- client/src/pages/ListingsPage.js
+++ client/src/pages/ListingsPage.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
 
+import ListingCard from "../components/ListingCard";
+
 function ListingsPage() {
   return (
     <div>
-      <p>This is the Listings page (it could alternatively be called the My Group page or something,
-      and have other info about the group the user is in)</p>
-      <p>You can view all the items that are available to borrow in your group here</p>
+      <p>This is the Listings page</p>
+      <p>You can view all the items that are available to borrow in your building here</p>
       <p>Also, there will be a link to lend a new item</p>
+      <ListingCard />
+      <ListingCard />
     </div>
   );
 }
