8ed2153ebd14bf77aa105957bf80f3b31c7f7b8e
chystarr <chynnastarr2@gmail.com>
2023-03-06 11:44:45 -0500

    Made some of the routes for the listings contoller and also changed one of the column names in the listings table

--- api/controllers/listings.js
+++ api/controllers/listings.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../models");
-const { Listing } = db;
+const { Listing, ItemType } = db;
 
 // Routes
 // 
@@ -16,5 +16,33 @@ const { Listing } = db;
 //
 // PATCH /api/listing/borrow
 // Borrow a listing
+
+router.get("/", (req, res) => {
+  Listing.findAll({}).then((allListings) => res.json(allListings));
+});
+
+/*
+router.get("/item-type/:id", async (req, res) => {
+  const { id } = req.params;
+  const typeWithId = await ItemType.findByPk(id);
+  if (!typeWithId) {
+    return res.sendStatus(404);
+  }
+});
+*/
+
+// maybe modify this so that building_id doesn't have to be a param in the body
+// only allow user to do this if she's already a member of the building?
+router.post("/", (req, res) => {
+  const { name, compensation, range_start, range_end, condition, item_description, building_id, item_type_id } = req.body;
+  const lender_id = 1; // placeholder until user auth is added
+  Listing.create({ name, compensation, range_start, range_end, condition, item_description, building_id, lender_id, item_type_id })
+  .then((newListing) => {
+    res.status(201).json(newListing);
+  })
+  .catch((err) => {
+    res.status(400).json(err);
+  });
+});
 
 module.exports = router;
