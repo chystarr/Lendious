4dbd80ba86bea5ed8fa031e3d9b538a41ae019f7
michaelxie1 <michaelxie12345@gmail.com>
2023-03-13 10:30:46 -0400

    Added a few fnctions for interactivity

--- client/src/pages/AboutPage.js
+++ client/src/pages/AboutPage.js
@@ -1,12 +1,23 @@
 import React, { useState, useEffect } from "react";
 
+function LearnMore(props) {
+  return (
+    <a href="HomePage.js">
+    <button onClick={props.onClick}>
+      Learn More
+    </button></a>
+  );
+}
+
 function AboutPage() {
   return (
     <div>
      <h1>Why Lendious</h1>
-     <p>Get access to a diverse range of objects without breaking the bank. With this platform, 
-      you can lend and borrow as you please while forming tighter community connections</p>
-      <button>Learn More</button>
+      <p>
+        Get access to a diverse range of objects without breaking the bank. With this platform, 
+        you can lend and borrow as you please while forming tighter community connections
+      </p>
+      <LearnMore />
     </div>
   );
 }
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,11 +1,31 @@
 import React, { useState, useEffect } from "react";
 
+
+
+function LoginButton(props) {
+  return (
+    <a href="HomePage.js">
+    <button onClick={props.onClick}>
+      Login
+    </button></a>
+  );
+}
+
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  );
+}
+
 function HomePage() {
   return (
     <div>
       <p>Welcome to Lendious</p>
       <p>Empowering communities through shared ownership: Lend what you don't use, use what you need</p>
-      <button>Sign Up</button>
+      <LoginButton />
+      <LogoutButton />
     </div>
   );
 }
