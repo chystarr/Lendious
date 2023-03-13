78940b398a56683c9e04c9bc48de41b60af2164d
ArunSaini09 <arunsaini0918@gmail.com>
2023-03-12 21:28:54 -0400

    import auth controller

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 // Load each controller
 const microPostsController = require("./microPosts.js");
+const authController = require("./auth.js");
 
 // Mount each controller under a specific route. These
 // will be prefixes to all routes defined inside the controller
