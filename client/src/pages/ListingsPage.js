dd497b52425e549b58eeabcff5cea3c01f6353ba
ArunSaini09 <arunsaini0918@gmail.com>
2023-03-19 18:33:43 -0400

    create dummy listingCard that displays data

--- client/src/pages/ListingsPage.js
+++ client/src/pages/ListingsPage.js
@@ -1,16 +1,49 @@
 import React, { useState, useEffect } from "react";
-
+import LoadingSpinner from "../components/LoadingSpinner";
+import ErrorAlert from "../components/ErrorAlert";
 import ListingCard from "../components/ListingCard";
 import AddListingButton from "../components/AddListingButton";
 
 function ListingsPage() {
-  return (
 
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      setLoading(true);
+      try {
+        let response = await fetch("/api/listings");
+        let allListings = await response.json();
+        setListings(allListings);
+        setLoading(false);
+
+      } catch (error){
+        console.log("Error fetching listings");
+        setError(true);
+      }
+    }
+
+    getData();
+
+    return () => {
+      //clean up function
+    };
+  }, []);
+
+  if(error) return <ErrorAlert details="Failed to fetch all listings" />;
+  if(loading) return <LoadingSpinner/>;
+
+
+  return (
     <div className="container-fluid text-center">
 			<div className="row justify-content-center">
         <AddListingButton/>
-        <ListingCard />
-        <ListingCard />
+        {console.log(listings)}
+        {listings.map((listing) => {
+          return <ListingCard {...listing} key = {listing.id} />
+        })}
       </div>
     </div>
   );
