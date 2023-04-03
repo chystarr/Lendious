6a0dda2f9bd5687dda00397010fbb6fb6cd29f62
michaelxie1 <michaelxie12345@gmail.com>
2023-04-03 11:59:22 -0400

    Still need to add some basic functionality to front-end (Will look into bootstrap framework too)

--- client/src/pages/ListingsPage.js
+++ client/src/pages/ListingsPage.js
@@ -2,6 +2,25 @@ import React, { useState, useEffect } from "react";
 
 import ListingCard from "../components/ListingCard";
 
+function BorrowItem(props) {
+  return (
+    <a href="HomePage.js">
+    <button onClick={props.onClick}>
+      Lend It
+    </button></a>
+  );
+}
+
+function LendOut(props) {
+  return (
+    <button onClick={props.onClick}>
+      Lend Out Item
+    </button>
+  );
+}
+
+
+
 function ListingsPage() {
   return (
     <div>
@@ -10,8 +29,10 @@ function ListingsPage() {
       <p>Also, there will be a link to lend a new item</p>
       <ListingCard />
       <ListingCard />
+    
     </div>
   );
 }
+
 
 export default ListingsPage;
