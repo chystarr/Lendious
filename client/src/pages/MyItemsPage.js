6d680e85cc3d40c6b25ea1b265293845ab576d61
chystarr <chynnastarr2@gmail.com>
2023-04-17 06:53:18 -0400

    Made endpoint to get items user is borrowing and made the user's borrowed items be displayed on the My Items page

--- client/src/pages/MyItemsPage.js
+++ client/src/pages/MyItemsPage.js
@@ -8,7 +8,9 @@ function MyItemsPage() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [listings, setListings] = useState([]);
+  const [borrowedListings, setBorrowedListings] = useState([]);
   const [searchResults, setSearchResults] = useState([]);
+  const [borrowedSearchResults, setBorrowedSearchResults] = useState([]);
   
   useEffect(() => {
     async function getData() {
@@ -17,6 +19,7 @@ function MyItemsPage() {
         let response = await fetch("/api/auth/login");
         let user = await response.json();
         getPersonalListings(user.user_id);
+        getBorrowedListings();
       } catch (error){
         console.log("Error fetching user inside myitems page");
         setError(true);
@@ -46,20 +49,44 @@ function MyItemsPage() {
     }
   }
 
+  async function getBorrowedListings()
+  {
+    try {
+      let response = await fetch("/api/listings/borrowing");
+      console.log("getting the user's borrowed listings:")
+      console.log(response);
+      let userListings = await response.json();
+      setBorrowedListings(userListings);
+      setBorrowedSearchResults(userListings);
+      setLoading(false);
+    } catch (error) {
+        console.log("Error getting user listings after completing user id fetch");
+        setError(true);
+    }
+  }
+
   if(error) return <ErrorAlert details="Failed to fetch user listings" />;
   if(loading) return <LoadingSpinner/>
 
   const results = searchResults.map((listing) => {
       return <ListingCard {...listing} key = {listing.listing_id} />
   })
+  const borrowedResults = borrowedSearchResults.map((listing) => {
+    return <ListingCard {...listing} key = {listing.listing_id} />
+})
 
  const content = results?.length ? results : <p className="mt-3">No Listings</p>
+ const borrowedContent = borrowedResults?.length ? borrowedResults : <p className="mt-3">No Listings</p>
 
   return (
     <div className="container-fluid text-center">
 			<div className="row justify-content-center">
+        <h3>Items I'm Lending</h3>
         <SearchBar listings={listings} setSearchResults={setSearchResults}/>
         {content}
+        <h3>Items I'm Borrowing</h3>
+        <SearchBar borrowedListings={borrowedListings} setSearchResults={setSearchResults}/>
+        {borrowedContent}
       </div>
     </div>
   );
