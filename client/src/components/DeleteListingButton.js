08d6c12db954d6c4ffb6e9ea7323dd4fb202a17d
chystarr <chynnastarr2@gmail.com>
2023-05-01 11:30:18 -0400

    Adjusted button formatting

--- client/src/components/DeleteListingButton.js
+++ client/src/components/DeleteListingButton.js
@@ -13,7 +13,7 @@ function DeleteListingButton({listing_id}) {
   return (
     <div className="col-10 col-md-8 col-lg-7">
       <div className="card mb-4 shadow">
-        <button type="button" onClick={handleClick} className="btn btn-primary btn-block"> Delete </button>
+        <button type="button" onClick={handleClick} className="btn btn-primary mx-auto"> Delete </button>
       </div>
     </div>
   );
