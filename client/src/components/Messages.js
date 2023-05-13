4afc03a39e1a584b097bb37053fe16f08e8ca1b5
chystarr <chynnastarr2@gmail.com>
2023-05-13 13:33:28 -0400

    API call is made to retrieve past messages upon entering a chatroom

--- client/src/components/Messages.js
+++ client/src/components/Messages.js
@@ -1,7 +1,30 @@
 import { useEffect, useState } from 'react';
 
-const Messages = ({ socket }) => {
+const Messages = ({ socket, listing_id }) => {
   const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    // retrieve all past messages for this chatroom upon entering
+    const getPastMessages = async () => {
+      try {
+        let response = await fetch("/api/messages/" + listing_id);
+        let pastMessages = await response.json();
+        console.log(pastMessages);
+  
+        if (response.ok) {
+          console.log("Retreived past messages");
+        }
+  
+      } catch(error) {
+        console.error("Past messages couldn't be retrieved", error);
+      }
+    };
+
+    getPastMessages();
+
+    return () => {
+    };
+  }, []);
 
   // updates the messages displayed whenever the client receives an "msg" event emitted by the server
   useEffect(() => {
--- client/src/pages/ChatPage.js
+++ client/src/pages/ChatPage.js
@@ -22,7 +22,7 @@ const ChatPage = () => {
   return (
     <div>
       <p>Chat page</p>
-      <Messages socket={socket} />
+      <Messages socket={socket} listing_id={params.listing_id} />
       <MessageInput socket={socket} listing_id={params.listing_id} />
     </div>
   );
