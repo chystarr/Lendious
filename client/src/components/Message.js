fe3c071adc1826c00bcb3fcbed2f72dd5ba2bbf1
chystarr <chynnastarr2@gmail.com>
2023-05-15 13:06:01 -0400

    Updated index.html title and favicon, and updated message component css

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -44,9 +44,9 @@ const Message = ({ message }) => {
   
 
   return (
-    <div className="w-60 border p-3 m-3">
+    <div id="message" className="card border-dark w-60 p-3 m-3">
       <div className="row">
-        <div className="col">{message.sender_name}</div>
+        <div className="col">{message.sender_name || "Name"}</div>
         <div className="col-9"></div>
         <div className="col"></div>
       </div>
