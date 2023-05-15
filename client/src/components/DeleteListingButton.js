7a2f7079c491556843060f7a7e037196ac527273
ArunSaini09 <arunsaini0918@gmail.com>
2023-05-14 23:23:10 -0400

    made page refresh on listing deletion

--- client/src/components/DeleteListingButton.js
+++ client/src/components/DeleteListingButton.js
@@ -1,13 +1,28 @@
-import React from 'react'
-import { Link } from "react-router-dom";
+import React, {useState} from 'react'
 
 function DeleteListingButton({listing_id}) {
+
+  const [error, setError] = useState(false);
+
   const handleClick = async (event) => {
     event.preventDefault();
-    let response = await fetch("/api/listings/" + listing_id, {
-      method: "DELETE",
-      credentials: "include",
-    });
+    try {
+      let response = await fetch("/api/listings/" + listing_id, {
+        method: "DELETE",
+        credentials: "include",
+      });
+
+      if (!response.ok) {
+        setError(true);
+      } else {
+        // Reload the page
+        window.location.reload();
+      }
+
+    } catch (error) {
+      console.log("error deleting a listing");
+    }
+    
   }
 
   return (
