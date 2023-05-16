57230eeef59dcaf33cc1ff620a29874b8938796c
michaelxie1 <michaelxie12345@gmail.com>
2023-05-16 17:27:51 -0400

    Made it so that when the user searches for items it will be case insensitive

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -5,33 +5,37 @@ const SearchBar = ({listings, setSearchResults, flag}) => {
 
 
     const handleSearchChange = (e) => {
-        if(!e.target.value) return setSearchResults(listings)
-
+        const searchValue = e.target.value.toLowerCase();
+      
+        if (!searchValue) {
+          setSearchResults(listings);
+          return;
+        }
+      
         let results = [];
-
-        if(flag)
-        {
-            results = listings.filter(listing => listing.name.includes(e.target.value));
-            setSearchResults(results)
+      
+        if (flag) {
+          results = listings.filter(listing => listing.name.toLowerCase().includes(searchValue));
+          setSearchResults(results);
         } else {
-            if(e.target.value.includes("Book"))
-            {
-                results = listings.filter(listing => listing.item_type_id === 3)
-            } else if (e.target.value.includes("Tool"))
-            {
-                results = listings.filter(listing => listing.item_type_id === 1)
-            } else if (e.target.value.includes("Game"))
-            {
-                results = listings.filter(listing => listing.item_type_id === 2)  
-            } else {
-                results = listings.filter(listing => listing.name.includes(e.target.value) ||
-                listing.condition.includes(e.target.value) || listing.range_start.includes (e.target.value)
-                || listing.range_end.includes(e.target.value))  
-            }
-
-            setSearchResults(results);    
-        }    
-    }
+          if (searchValue.includes("book")) {
+            results = listings.filter(listing => listing.item_type_id === 3);
+          } else if (searchValue.includes("tool")) {
+            results = listings.filter(listing => listing.item_type_id === 1);
+          } else if (searchValue.includes("game")) {
+            results = listings.filter(listing => listing.item_type_id === 2);
+          } else {
+            results = listings.filter(listing =>
+              listing.name.toLowerCase().includes(searchValue) ||
+              listing.condition.toLowerCase().includes(searchValue) ||
+              listing.range_start.toLowerCase().includes(searchValue) ||
+              listing.range_end.toLowerCase().includes(searchValue)
+            );
+          }
+      
+          setSearchResults(results);
+        }
+      }
     return(
         <div className="col-10 col-md-8 col-lg-7">
             <div className="input-group rounded">
