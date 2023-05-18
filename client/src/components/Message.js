d45ebfa8f844d897b49784d3df1f1439181f7ba5
chystarr <chynnastarr2@gmail.com>
2023-05-18 17:48:22 -0400

    Past messages retrieved from database now show name of sender

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -1,52 +1,39 @@
 import { useState, useEffect } from 'react';
 
 const Message = ({ message }) => {
-  /*
   const [name, setName] = useState("");
 
   useEffect(() => {
-    async function getData () {
-      try {
-        let nameResponse = await fetch("/api/users/" + message.sender_id + "/name");
-        let senderName = await nameResponse.json();
-        setName(senderName);
-      } catch (error) {
-        console.error("Error identifying sender", error);
+    // If message.sender_id is truthy, then the message was a past message that was rendered upon
+    // the user entering the chatroom
+    if (message.sender_id) {
+      async function getData () {
+        try {
+          let nameResponse = await fetch("/api/users/user/" + message.sender_id + "/name");
+          let senderName = await nameResponse.json();
+          setName(senderName);
+        } catch (error) {
+          console.error("Error identifying sender", error);
+        }
       }
+
+        if (message.sender_id) {
+          getData();
+        }
+
+      return () => {
+      };
     }
-
-      if (message.sender_id) {
-        getData();
-      }
-
-    return () => {
-    };
   }, []);
   
-  
-  if (message.sender_id) {
-    // message was loaded from the database
-    return (
-      <div className="w-60 border p-3 m-3">
-        <div className="row">
-          <div className="col">{name}</div>
-          <div className="col-9"></div>
-          <div className="col">time</div>
-        </div>
-        <br />
-        <div className="row">
-          <p>{message.message_content}</p>
-        </div>
-      </div>
-    )
-  }
-  */
-  
+  console.log(message);
 
   return (
     <div id="message" className="card border-dark w-60 p-3 m-3">
       <div className="row">
-        <div className="col">{message.sender_name || "Name"}</div>
+        {/* If message.sender_name is truthy, then the message was sent by the server while the user was
+        in the chatroom */}
+        <div className="col">{message.sender_name || name}</div>
         <div className="col-9"></div>
         <div className="col"></div>
       </div>
