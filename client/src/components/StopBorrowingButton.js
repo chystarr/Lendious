004d6a60fc2f7e87070305e1b54e03d0e048f92c
chystarr <chynnastarr2@gmail.com>
2023-05-18 20:12:51 -0400

    Added functionality for user to stop borrowing an item to StopBorrowingButton component

--- client/src/components/StopBorrowingButton.js
+++ client/src/components/StopBorrowingButton.js
@@ -1,9 +1,35 @@
 import React, {useState} from 'react'
 
 function StopBorrowingButton({listing_id}) {
+  const [error, setError] = useState(false);
+
+  const handleClick = async (event) => {
+    event.preventDefault();
+    try {
+      let response = await fetch("/api/listings/" + listing_id + "/stop-borrowing", {
+        method: "PATCH",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        }
+      });
+
+      if (!response.ok) {
+        setError(true);
+      } else {
+        // Reload the page
+        window.location.reload();
+      }
+
+    } catch (error) {
+      console.log("Server error when user tried to stop borrowing an item");
+    }
+    
+  }
+
   return (
     <div className="col-10 col-md-8 col-lg-7 mt-3">
-        <button type="button" className="btn btn-primary mx-auto"> Stop Borrowing </button>
+      <button type="button" onClick={handleClick} className="btn btn-primary mx-auto"> Stop Borrowing </button>
     </div>
   );
 };
